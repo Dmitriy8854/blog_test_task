d779c47748256bb2c7d5dfb39bca9a803de6bd72
Dmitriy8854 <dmit.morozoff2009@yandex.ru>
2023-08-17 23:27:25 +0300

    Setting celery

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -140,3 +140,5 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ],
 }
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
